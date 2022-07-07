50b2235f77bc14716b28c0aae2bb72ea49ef3b3b
Appy-arsnel <75736454+Appy-arsnel@users.noreply.github.com>
2022-07-07 18:36:35 +0530

    Commit

---
+++ Assets/Scripts/particle_start.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class particle_start : MonoBehaviour
+{   ParticleSystem myp;
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        myp= GetComponent<ParticleSystem>();
+        myp.Play();
+    }
+
+   
+}
